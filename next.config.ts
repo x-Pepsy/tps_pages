846904ecd6dbdd7a33f95de8f8db4fff6cd9460e
PsyMan47 <xhonirexhepi@gmail.com>
2025-11-19 01:06:46 +0100

    Remove distDir from next.config to allow default export to out

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/tps_pages',
-  distDir: 'out',
   images: {
     unoptimized: true,
   },
